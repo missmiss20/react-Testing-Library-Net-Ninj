6dfda168deec6981c31fb7bffb5086b90e44c743
Jerry <tcjwang@outlook.com>
2023-12-16 12:22:37 -0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,5 +4,13 @@ import "./Header.css"
 export default function Header({
     title
 }) {
-    return <h1 className="header">{title}</h1>
+    return (    
+    <>
+        <h1 className="header">{title}</h1>
+        {/* tutorial content below */}
+        <h3 title='head' className="header">Cats</h3>    
+        <h3 data-testid='1' className="header">Dogs</h3>    
+        {/* end of tutorial content */}
+    </>
+    )
 }
